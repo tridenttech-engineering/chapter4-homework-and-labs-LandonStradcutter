8fdc89f0a5fc06c78066a240805ce6799ccdf5ac
LandonStradcutter <Stradcutter.landon@gmail.com>
2025-02-11 23:35:06 -0500

    Update hw4-14.cpp

--- hw4-14.cpp
+++ hw4-14.cpp
@@ -5,7 +5,21 @@
 using namespace std;
 
 int main() {
-  
+double small = 0.0;
+double medium = 0.0;
+double large = 0.0;
+double family = 0.0;
+double totalSold = 0.0;
+cout << "Small pizzas sold: ";
+cin >> small;
+cout << "Medium pizzas sold: ";
+cin >> medium;
+cout << "Large pizzas sold: ": cin > larges
+cout << Family pizzas sold: *; cin » family;
+totalSold - small + medium + large + fandly: cout « endi;
+cout << Total pizzas sold: • « totalSold « endi;
+cout << "Small pizzas sold: • « small/ totalSold • 100 « -g° « end; cout « "Medium pizzas sold: - « mediun/ totalSold • 100 « '%* « endl; cout « Large pizzas sold:* « large/totalSold • 100 « "g" « endi; cout « "Family pizzes sold: " « feuday/ totalSold • 100 « *%* «€ endia
+
 
   return 0;
 } // end of main function
